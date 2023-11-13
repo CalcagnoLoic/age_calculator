fe6a9a54a320bebd5847b1c595d7e71ba6a74a45
Loïc Calcagno <calcagnoloic93@gmail.com>
2023-11-13 14:11:50 +0100

    add font in the config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,22 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["index.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+const defaultTheme = require("tailwindcss/defaultTheme");
 
+module.exports = {
+    content: ["index.html"],
+    theme: {
+        extend: {
+            colors: {
+                purple: "hsl(259, 100%, 65%)",
+                ligth_red: "hsl(0, 100%, 67%)",
+                off_white: "hsl(0, 0%, 94%)",
+                light_grey: "hsl(0, 0%, 86%)",
+                smokey_grey: "hsl(0, 1%, 44%)",
+                off_black: "hsl(0, 0%, 8%)",
+            },
+            fontFamily: {
+                sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+    plugins: [],
+};
